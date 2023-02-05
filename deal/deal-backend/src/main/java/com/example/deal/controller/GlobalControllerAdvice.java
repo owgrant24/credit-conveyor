3759fd05f3d52be9c9d9184311b3b2842a0f4412
ashabelskii <ashabelskii@neoflex.ru>
2023-02-05 21:32:12 +0300

    deal-backend add GlobalControllerAdvice

---
+++ deal/deal-backend/src/main/java/com/example/deal/controller/GlobalControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.example.deal.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<String> handleAllExceptions(Exception e) {
+        String stackTrace = ExceptionUtils.getStackTrace(e);
+        log.error(stackTrace);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
