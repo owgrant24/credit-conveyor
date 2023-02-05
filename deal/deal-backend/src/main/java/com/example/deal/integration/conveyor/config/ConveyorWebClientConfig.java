46f2b82f7372048893d0a0c21b6c6878b81a344c
ashabelskii <ashabelskii@neoflex.ru>
2023-02-05 13:53:30 +0300

    deal-backend add logging (logbook)

--- deal/deal-backend/src/main/java/com/example/deal/integration/conveyor/config/ConveyorWebClientConfig.java
+++ deal/deal-backend/src/main/java/com/example/deal/integration/conveyor/config/ConveyorWebClientConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.netty.LogbookClientHandler;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
@@ -15,14 +17,16 @@ import java.time.Duration;
 public class ConveyorWebClientConfig {
 
     @Bean
-    public WebClient conveyorWebClient(ConveyorConfig config) {
+    public WebClient conveyorWebClient(ConveyorConfig config, Logbook logbook) {
         ConveyorRestClientConfig clientConfig = config.getClient();
+
 
         HttpClient httpClient = HttpClient.create()
                 .responseTimeout(Duration.ofSeconds(clientConfig.getConnectTimeout()))
-                .doOnConnected(conn -> conn
+                .doOnConnected(connection -> connection
                         .addHandlerLast(new ReadTimeoutHandler(clientConfig.getReadTimeout()))
-                        .addHandlerLast(new WriteTimeoutHandler(clientConfig.getWriteTimeout())));
+                        .addHandlerLast(new WriteTimeoutHandler(clientConfig.getWriteTimeout()))
+                        .addHandlerLast(new LogbookClientHandler(logbook)));
 
         ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
 
